e97d0196beecf5ccffa42901f6f8e49cc7369ab1
LustXcc <lustxcc@gmail.com>
2016-12-08 19:49:33 +0800

    1208第一次提交

---
+++ indicatorView/UIView+PCExtension.h
@@ -0,0 +1,23 @@
+//
+//  UIView+PCExtension.h
+//  完整项目
+//
+//  Created by Skylab on 16/9/18.
+//  Copyright © 2016年 Skylab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (PCExtension)
+
+@property (nonatomic, assign) CGSize size;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+
+
+@end
+ 
---
+++ indicatorView/ViewController.m
@@ -0,0 +1,99 @@
+//
+//  ViewController.m
+//  indicatorView
+//
+//  Created by LustXcc on 08/12/2016.
+//  Copyright © 2016 LustXcc. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "UIView+PCExtension.h"
+
+@interface ViewController ()
+/** 红色指示器 */
+@property (nonatomic, weak) UIView *indicatorView;
+/** 当前选中的按钮 */
+@property (nonatomic, weak) UIButton *selectedButton;
+/** 所有标签 */
+@property (nonatomic, weak) UIView *titlesView;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupIndicator];
+}
+
+
+- (void)setupIndicator
+{
+    // 标签栏整体
+    UIView *titlesView = [[UIView alloc] init];
+    titlesView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:1];
+    titlesView.width = self.view.width;
+    titlesView.height = 35;
+    titlesView.y = 64;
+    [self.view addSubview:titlesView];
+    self.titlesView = titlesView;
+    
+    // 底部的红色指示器
+    UIView *indicatorView = [[UIView alloc] init];
+    indicatorView.backgroundColor = [UIColor redColor];
+    indicatorView.height = 2;
+    indicatorView.tag = -1;
+    indicatorView.y = titlesView.height - indicatorView.height;
+    self.indicatorView = indicatorView;
+    
+    // 内部的子标签
+    NSArray *titles = @[@"推荐", @"视频", @"周边", @"图片", @"手机"];
+    CGFloat width = titlesView.width / titles.count;
+    CGFloat height = titlesView.height;
+    for (NSInteger i = 0; i<titles.count; i++) {
+        UIButton *button = [[UIButton alloc] init];
+        button.tag = i;
+        button.height = height;
+        button.width = width;
+        button.x = i * width;
+        [button setTitle:titles[i] forState:UIControlStateNormal];
+        //        [button layoutIfNeeded]; // 强制布局(强制更新子控件的frame)
+        [button setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor redColor] forState:UIControlStateDisabled];
+        button.titleLabel.font = [UIFont systemFontOfSize:14];
+        [button addTarget:self action:@selector(titleClick:) forControlEvents:UIControlEventTouchUpInside];
+        [titlesView addSubview:button];
+        
+        // 默认点击了第一个按钮
+        if (i == 0) {
+            button.enabled = NO;
+            self.selectedButton = button;
+            
+            // 让按钮内部的label根据文字内容来计算尺寸
+            [button.titleLabel sizeToFit];
+            self.indicatorView.width = button.titleLabel.width;
+            self.indicatorView.centerX = button.centerX;
+        }
+    }
+    
+    [titlesView addSubview:indicatorView];
+    
+}
+
+
+- (void)titleClick:(UIButton *)button
+{
+    // 修改按钮状态
+    self.selectedButton.enabled = YES;
+    button.enabled = NO;
+    self.selectedButton = button;
+    
+    // 动画
+    [UIView animateWithDuration:0.25 animations:^{
+        self.indicatorView.width = button.titleLabel.width;
+        self.indicatorView.centerX = button.centerX;
+    }];
+    
+}
+@end
+
+
